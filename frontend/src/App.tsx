55b65c69c87c54c4fa8a9ac24b4cb4ab8f284f92
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-13 15:47:54 +0500

    Added Signin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css'
 import Landing from './components/Landing'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import Signin from './components/Signin';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCijoVcwkeJe4niBkBd8n07Fe1JhQfxu0E",
+  authDomain: "leetcode-clone-9205a.firebaseapp.com",
+  projectId: "leetcode-clone-9205a",
+  storageBucket: "leetcode-clone-9205a.appspot.com",
+  messagingSenderId: "212689169262",
+  appId: "1:212689169262:web:b77d2cbddd8d7b0995af7f",
+  measurementId: "G-HM6Y83BJ75"
+};
+
+const app = initializeApp(firebaseConfig);
+
 
 function App() {
   
   return (
     <div className='text-3xl font-bold underline'>
       <Landing />
+      <Signin />
     </div>
   )
 }
