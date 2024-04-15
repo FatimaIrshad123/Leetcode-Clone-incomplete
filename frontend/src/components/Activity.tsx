7a0f8c429261c5827513f4d7def1ddb9a0c930e4
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-15 12:16:29 +0500

    Added activity

--- frontend/src/components/Activity.tsx
+++ frontend/src/components/Activity.tsx
@@ -3,9 +3,15 @@ import { SubmissionActivityList } from "./SubmissionActivityList";
 
 export default function Activity(){
     return (
-        <div>
-            <SubmissionActivity />
+        <div className="flex flex-col md:flex-row mt-5 ">
+            <div className="mr-5">
             <SubmissionActivityList />
+            </div>
+            <div className="border pt-2 border-2xl rounded-lg md:w-64">
+            <SubmissionActivity />
+            </div>
+            
+            
         </div>
     )
 }
