4cdec0b97b5ac19a4f2430fd2bc297f88fd6c27b
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-16 20:06:43 +0500

    Added mongodb Schema

---
+++ backend/src/schema.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://Fatima:Fatima@cluster0.7bunznc.mongodb.net/LeetCode-Clone')
+
+const problemSchema = new mongoose.Schema({
+    defaultCode: {
+        cpp: String,
+        js: String
+    },
+    description: String,
+    testCases: [{
+        input: String,
+        output: String
+    }],
+    title: String
+})
+
+const submissionSchema = new mongoose.Schema({
+    language: String,
+    problemId: String,
+    status: String,
+    submission: String,
+    submitTime: Number,
+    userId: String,
+    workerPickTime: Number,
+    workerTryCount: Number
+})
+
+const userSchema = new mongoose.Schema({
+    points: Number
+})
+const dailyActivitySchema = new mongoose.Schema({
+    count: Number
+})
+
+const Problems = mongoose.model('Problems',problemSchema);
+const Submissions = mongoose.model('Submissions',submissionSchema);
+const User = mongoose.model('User',userSchema);
+const DailyActivity = mongoose.model('DailyActivity',dailyActivitySchema)
+
+module.exports = {
+    Problems,
+    Submissions,
+    User,
+    DailyActivity
+}
